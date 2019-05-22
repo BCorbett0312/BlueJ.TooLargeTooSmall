228f39b17e9e94791bc2a532919ac9295bc2adb7
Benjamin Corbett <benjamincorbett@zipcodersMBP37.localdomain>
2019-05-22 07:47:51 -0400

    submit files

--- Main.java
+++ Main.java
@@ -5,29 +5,61 @@
  * @author (your name)
  * @version (a version number or a date)
  */
-public class Main
-{
-    // instance variables - replace the example below with your own
-    private int x;
 
-    /**
-     * Constructor for objects of class Main
-     */
-    public Main()
-    {
-        // initialise instance variables
-        x = 0;
-    }
+import java.util.Random;
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args ){
+        // instance variables - replace the example below with your own
+        Scanner in = new Scanner(System.in);
+        Random correctAnswer = new Random();
+        int numberToGuess = correctAnswer.nextInt(26);
+        int numberOfGuesses = 0;
+        boolean win = false;
+        int prevGuess = 100;
+    
+        
+        
+    
+    
+    
+        while (win == false){
+    
+        
+            System.out.print("Please guess a number between 0 and 25");
+            int guess = in.nextInt();
+            
+            
+            if (prevGuess != guess) {numberOfGuesses++;}
+            prevGuess = guess;
+    
+            if (guess == numberToGuess) {win = true;}
+    
+    
+            else if (guess > numberToGuess) {System.out.println("Too High");}
+    
+    
+            else if (guess < numberToGuess) {System.out.println("Too Low");}
+    
+            
+            
+            
+            
+                //  if (guess != prevGuess) {numberOfGuesses++;}
+                // initialise instance variables
+        
+    
+        
+    
+        
+    
+    
+    
+        }
 
-    /**
-     * An example of a method - replace this comment with your own
-     *
-     * @param  y  a sample parameter for a method
-     * @return    the sum of x and y
-     */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+        System.out.println("Correct Guess");
+        System.out.println("The number was " + numberToGuess);
+        System.out.println("It took you " + numberOfGuesses + " tries");
+ 
     }
 }
